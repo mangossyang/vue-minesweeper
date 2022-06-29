4a9a83d6924f66fb237b7fed6464af83d2a26e89
sunyangming <1484824012@qq.com>
2022-06-29 21:45:35 +0800

    wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,49 +1,133 @@
 <script setup lang="ts">
-const name = $ref('')
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean // 是否点开
+  mine?: boolean // 地雷
+  flagged?: boolean // 标记
+  adjacentMines: number
+}
+const WIDTH = 10
+const HEIGHT = 10
+const state = reactive(
+  Array.from({ length: HEIGHT }, (_, y) => (
+    Array.from({ length: WIDTH }, (_, x): BlockState => (
+      {
+        x,
+        y,
+        adjacentMines: 0,
+        revealed: false,
+      }))
+  )),
+)
+let mineGenerator = false
+const dev = true
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-pink-500',
+  'text-orange-500',
+  'text-purple-500',
+]
+const getSiblings = (block: BlockState) => {
+  return directions.map(([dx, dy]) => {
+    const x2 = block.x + dx
+    const y2 = block.y + dy
+    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+      return undefined
+    return state[y2][x2]
+  }).filter(Boolean) as BlockState[]
+}
+const updateNumbers = () => {
+  state.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine)
+        return
+      getSiblings(block).forEach((b) => {
+        if (b.mine)
+          block.adjacentMines += 1
+      })
+    })
+  })
+}
+const expendZero = (block: BlockState) => {
+  if (block.adjacentMines)
+    return
+
+  getSiblings(block).forEach((b) => {
+    if (!b.revealed) {
+      b.revealed = true
+      expendZero(b)
+    }
+  })
+}
+const generatorMines = (initial: BlockState) => {
+  for (const row of state) {
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) <= 1 || Math.abs(initial.y - block.y) <= 1)
+        continue
+      block.mine = Math.random() < 0.2
+    }
+  }
+  updateNumbers()
+}
+const onClick = (block: BlockState) => {
+  if (!mineGenerator) {
+    generatorMines(block)
+    mineGenerator = true
+  }
+  if (block.mine)
+    alert('boom!!')
+
+  block.revealed = true
+  expendZero(block)
+}
+
+const getBlockClass = (block: BlockState) => {
+  if (!block.revealed)
+    return 'bg-gray-500/10'
+  return block.mine ? 'bg-red-500/20' : numberColors[block.adjacentMines]
 }
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
+  minesweeper
 
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
+  <div p5>
+    <div
+      v-for="row, y in state" :key="y"
+      flex="~"
+      items-center justify-center
     >
-
-    <div>
       <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
+        v-for="block, x in row" :key="x"
+        w-10 h-10 m=".5"
+        flex="~"
+        items-center justify-center
+        hover="bg-gray-500 10"
+        border="gray-500/10 1"
+        :class="getBlockClass(block)"
+        @click="onClick(block)"
       >
-        Go
+        <template v-if="block.revealed || dev">
+          <div v-if="block.mine" i-mdi:mine />
+          <div v-else>
+            {{ block.adjacentMines }}
+          </div>
+        </template>
       </button>
     </div>
   </div>
